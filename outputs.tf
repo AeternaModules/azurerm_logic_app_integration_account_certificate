819f85f84a87d0af8182174998dda41680e4638b
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:42:36 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "logic_app_integration_account_certificates" {
+  description = "All logic_app_integration_account_certificate resources"
+  value       = azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates
+}
+
