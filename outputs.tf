9c398aa1174c1d01f9dccff0e0608c6a5e32dc44
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "logic_app_integration_account_certificates" {
-  description = "All logic_app_integration_account_certificate resources"
-  value       = azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates
-}
 output "logic_app_integration_account_certificates_integration_account_name" {
-  description = "List of integration_account_name values across all logic_app_integration_account_certificates"
-  value       = [for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : v.integration_account_name]
+  description = "Map of integration_account_name values across all logic_app_integration_account_certificates, keyed the same as var.logic_app_integration_account_certificates"
+  value       = { for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : k => v.integration_account_name }
 }
 output "logic_app_integration_account_certificates_key_vault_key" {
-  description = "List of key_vault_key values across all logic_app_integration_account_certificates"
-  value       = [for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : v.key_vault_key]
+  description = "Map of key_vault_key values across all logic_app_integration_account_certificates, keyed the same as var.logic_app_integration_account_certificates"
+  value       = { for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : k => v.key_vault_key }
 }
 output "logic_app_integration_account_certificates_metadata" {
-  description = "List of metadata values across all logic_app_integration_account_certificates"
-  value       = [for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : v.metadata]
+  description = "Map of metadata values across all logic_app_integration_account_certificates, keyed the same as var.logic_app_integration_account_certificates"
+  value       = { for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : k => v.metadata }
 }
 output "logic_app_integration_account_certificates_name" {
-  description = "List of name values across all logic_app_integration_account_certificates"
-  value       = [for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : v.name]
+  description = "Map of name values across all logic_app_integration_account_certificates, keyed the same as var.logic_app_integration_account_certificates"
+  value       = { for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : k => v.name }
 }
 output "logic_app_integration_account_certificates_public_certificate" {
-  description = "List of public_certificate values across all logic_app_integration_account_certificates"
-  value       = [for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : v.public_certificate]
+  description = "Map of public_certificate values across all logic_app_integration_account_certificates, keyed the same as var.logic_app_integration_account_certificates"
+  value       = { for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : k => v.public_certificate }
 }
 output "logic_app_integration_account_certificates_resource_group_name" {
-  description = "List of resource_group_name values across all logic_app_integration_account_certificates"
-  value       = [for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : v.resource_group_name]
+  description = "Map of resource_group_name values across all logic_app_integration_account_certificates, keyed the same as var.logic_app_integration_account_certificates"
+  value       = { for k, v in azurerm_logic_app_integration_account_certificate.logic_app_integration_account_certificates : k => v.resource_group_name }
 }
 
